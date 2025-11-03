182d6daa6124aa583210ca28dce806ce57902ada
Tabarek <tabarekghassan4@gmail.com>
2025-11-03 13:58:00 +0300

    Revert "Refactor Podspec for no_screenshot plugin"
    This reverts commit 648cdc0072f4fb7c70c17ec880933c04197c1030.

--- ios/Classes/NoScreenshotPlugin.m
+++ ios/Classes/NoScreenshotPlugin.m
@@ -1,26 +1,15 @@
-#
-# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html.
-# Run `pod lib lint no_screenshot.podspec` to validate before publishing.
-#
-Pod::Spec.new do |s|
-  s.name             = 'no_screenshot'
-  s.version          = '0.3.2-beta.3'
-  s.summary          = 'Flutter plugin to enable, disable or toggle screenshot support in your application.'
-  s.description      = <<-DESC
-A new Flutter plugin project.
-                       DESC
-  s.homepage         = 'https://github.com/FlutterPlaza/no_screenshot'
-  s.license          = { :file => '../LICENSE' }
-  s.author           = { 'FlutterPlaza' => 'dev@flutterplaza.com' }
-  s.source           = { :path => '.' }
-  s.source_files = 'Classes/**/*'
-  s.dependency 'Flutter'
-  # Updated the dependency version to remove the wildcard and use a specific version range
-  s.dependency       'ScreenProtectorKit', '~> 1.3.1'
-  s.platform = :ios, '10.0'
+#import "NoScreenshotPlugin.h"
+#if __has_include(<no_screenshot/no_screenshot-Swift.h>)
+#import <no_screenshot/no_screenshot-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "no_screenshot-Swift.h"
+#endif
 
-  # Flutter.framework does not contain a i386 slice.
-  s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
-  # Updated swift_version to a single version as an array is not supported for this attribute
-  s.swift_version    = "5.0"
-end
+@implementation NoScreenshotPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [IOSNoScreenshotPlugin registerWithRegistrar:registrar];
+}
+@end
